357f4670692b002d93170d43af3a13aed0f7bf42
uday7g <uday7g@users.noreply.github.com>
2016-05-06 18:46:41 -0500

    sleep 60

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@ rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 yum -y install puppet
 echo "server = master.localdomain" >> /etc/puppet/puppet.conf
 service puppet start
-puppet agent -t
+sleep 60
+puppet agent -tdv
 puppet agetn -tdv
