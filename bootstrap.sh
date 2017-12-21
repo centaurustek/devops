84b2bf4913ffba4360a784c9cf2791b4442c90f9
naiduklr936 <naiduklr936@gmail.com>
2017-12-20 21:10:13 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 yum -y install puppet
+echo "Hello"
 echo "server = ec2-54-208-160-228.compute-1.amazonaws.com" >> /etc/puppet/puppet.conf
 service puppet start
 sleep 60
